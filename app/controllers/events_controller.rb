94ae807f936510b33688840f080c276c739d1673
George Chang <georgec1104@gmail.com>
2015-06-11 13:40:18 +0800

    added respond to for different file formats

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,12 +7,24 @@ before_action :set_event, :only => [ :show, :edit, :update, :destroy]
       def index
             #@events = Event.all
             @events = Event.page(params[:page]).per(20)
+            respond_to do |format|
+              format.html
+              format.xml { render :xml => @events.to_xml }
+              format.json { render :json => @events.to_json }
+              format.atom { @feed_title = "My event list" } # index.atom.builder
+            end
+
+
       end
+
 
 #get /events/new
       def new
             @event= Event.new
       end
+
+
+
 #POST /events/create
       def create
         @event = Event.new(event_params)
@@ -24,11 +36,24 @@ before_action :set_event, :only => [ :show, :edit, :update, :destroy]
       end
       end
 
+
+
+
 #GET /events/show/:id
       def show
 
             @page_title = @event.name
+            respond_to do |format|
+                format.html { @page_title = @event.name } # show.html.erb
+                format.xml # show.xml.builder
+                format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+            end
+
       end
+
+
+
+
 #GET /events/edit/:id
       def edit
 
---
+++ app/views/events/show.xml.builder
@@ -0,0 +1,5 @@
+xml.event do |e|
+  e.name @event.name
+  e.description @event.description
+
+end
