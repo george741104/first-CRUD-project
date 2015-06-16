079fe773c4f2250aa75990c6c469d6ee57492e64
George Chang <georgec1104@gmail.com>
2015-06-16 10:13:00 +0800

    added attendee model and database

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,3 +2,8 @@ class Attendee < ActiveRecord::Base
       validates_presence_of :name
       has_many :attendees,
 end
+ class Attendee < ActiveRecord::Base
+  belongs_to :event
+
+end
+
--- db/migrate/20150615031618_create_attendees.rb
+++ db/migrate/20150615031618_create_attendees.rb
@@ -1,7 +1,8 @@
 class CreateAttendees < ActiveRecord::Migration
   def change
     create_table :attendees do |t|
-
+      t.string :name
+      t.integer :event_id
       t.timestamps null: false
     end
   end
