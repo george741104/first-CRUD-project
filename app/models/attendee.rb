39a345692013f3388b3db2013711b5a0693dec09
George Chang <georgec1104@gmail.com>
2015-06-16 09:38:54 +0800

    added attendee and category model

---
+++ app/models/attendee.rb
@@ -0,0 +1,4 @@
+class Attendee < ActiveRecord::Base
+      validates_presence_of :name
+      has_many :attendees,
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610100241) do
+ActiveRecord::Schema.define(version: 20150616013533) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -22,7 +34,10 @@ ActiveRecord::Schema.define(version: 20150610100241) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "status"
+    t.integer  "category_id"
   end
+
+  add_index "events", ["category_id"], name: "index_events_on_category_id"
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
