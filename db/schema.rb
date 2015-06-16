894524439a68fe7c3b869527b34dccd02d046f5e
George Chang <georgec1104@gmail.com>
2015-06-16 11:02:41 +0800

    added location model and database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616013533) do
+ActiveRecord::Schema.define(version: 20150616030153) do
 
   create_table "attendees", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 20150616013533) do
   end
 
   add_index "events", ["category_id"], name: "index_events_on_category_id"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
