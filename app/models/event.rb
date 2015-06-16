b4cc6d8ab833857a9b0317914008f9c63f6ce457
George Chang <georgec1104@gmail.com>
2015-06-16 11:43:19 +0800

    added some advanced control commands to event.rb model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,16 @@
 class Event < ActiveRecord::Base
 
     validates_presence_of :name
-    has_many :attendees
+
+    has_many :attendees, :dependent => :destroy #Attendees would be deleted as well when destroying event.
+
+    has_many :attendees, -> {where(['created_at > ?', Time.now - 7.day]).order("id DESC")}
+    has_many :attendees, ->{ order("id DESC")}
     has_one :location
+
     has_many :event_groupships
-    has_many :goups, :through => :event_groupships
+
+    has_many :groups, :through => :event_groupships
+
+    has_many :attendees, -> {where(['created_at > ?', Time.now - 7.day]).order("id DESC")}
 end
