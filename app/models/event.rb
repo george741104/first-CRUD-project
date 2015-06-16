d11882053a04b898fffdcc3b9d0242c377bdfc60
George Chang <georgec1104@gmail.com>
2015-06-16 11:27:09 +0800

    added event_groupships and group models. Also connected all three models through has_many and belongs_to commands

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ActiveRecord::Base
     validates_presence_of :name
     has_many :attendees
     has_one :location
+    has_many :event_groupships
+    has_many :goups, :through => :event_groupships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616030153) do
+ActiveRecord::Schema.define(version: 20150616032224) do
 
   create_table "attendees", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150616030153) do
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_groupships", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -40,6 +47,12 @@ ActiveRecord::Schema.define(version: 20150616030153) do
   end
 
   add_index "events", ["category_id"], name: "index_events_on_category_id"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
